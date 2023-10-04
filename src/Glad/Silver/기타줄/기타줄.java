b16b3bff3504b7bcd92eba7d2ff1886ec513520e
0320kangk <0320kangk@naver.com>
2023-10-04 13:06:50 +0900

    기타줄 및 정사각형 코드 및 풀이

--- src/Glad/Silver/기타줄/기타줄.java
+++ src/Glad/Silver/기타줄/기타줄.java
@@ -1,2 +1,29 @@
-package Glad.Silver.기타줄;public class 기타줄 {
+package Glad.Silver.기타줄;
+
+import java.util.Scanner;
+
+public class 기타줄 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int minSet = 1000;
+        int minE = 1000;
+        for (int i = 0; i < M; i++) {
+            int set = sc.nextInt();
+            int e = sc.nextInt();
+            minSet = Math.min(set, minSet);
+            minE = Math.min(minE, e);
+        }
+        int total;
+        if(minSet / 6  < minE){
+            int sum1 = N / 6 * minSet + (N % 6) * minE;
+            //세트로만 사야하는 경우도있네
+            int sum2 = (int)Math.ceil(N / 6.0) * minSet;
+            total = Math.min(sum1, sum2);
+        } else {
+            total = minE * N;
+        }
+        System.out.println(total);
+    }
 }
--- src/Glad/Silver/숫자_정사각형/숫자_정사각형.java
+++ src/Glad/Silver/숫자_정사각형/숫자_정사각형.java
@@ -1,2 +1,49 @@
-package Glad.Silver.숫자_정사각형;public class 숫자_정사각형 {
+package Glad.Silver.숫자_정사각형;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 숫자_정사각형 {
+    public static void main(String[] args) {
+
+        /*
+        1. n m 값 행열 입력
+        2. n m 중 작은 값이 최대 정사각형 길이
+        3. 2부터 시작하여 4개의 꼭짓점을 확인
+         */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        String[] rec = new String[N];
+        for (int i = 0; i < N; i++) {
+            rec[i] = sc.next();
+        }
+        System.out.println(Arrays.toString(rec));
+        //4개의 꼭짓점 검사
+        int length = Math.min(N, M);
+        //길이 반복문 2부터..
+        for (int i = length - 1; i >= 1; i--) {
+            //길이에 대한 검사... 시작점을 정하기...
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    //한점을 기준으로 4점 찾기
+                    if ( j + i < N && k + i < M ){
+                        int dot1 = rec[j].charAt(k);
+                        int dot2 = rec[j + i].charAt(k);
+                        int dot3 = rec[j].charAt(k + i);
+                        int dot4 = rec[j + i].charAt(k + i);
+
+                        if (dot1 == dot2 &&
+                                dot1 == dot3 &&
+                                dot1 == dot4){
+                            System.out.println((i+1)*(i+1));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(1);
+
+    }
 }
