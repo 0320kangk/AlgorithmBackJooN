01fe9f6fab1a61529184b293c50097eba5bc1c22
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2024-06-17 17:54:19 +0900

    암호 코드

--- src/Glad/Gold/암호/암호.java
+++ src/Glad/Gold/암호/암호.java
@@ -7,17 +7,18 @@ public class 암호 {
         Scanner sc = new Scanner(System.in);
         String words = sc.next();
         String password = sc.next();
-        int answer = 0;
+        long answer = 0;
+        long multiplier = 1;
+        int module = 900528;
         for (int i = 0; i < password.length() - 1; i++) {
-            answer = (answer + words.length() *  (i + 1)) % 900528;
+            multiplier = (words.length() * multiplier) % module;
+            answer = (answer + multiplier) % module;
         }
-        for (int i = 0; i < password.length(); i++) {
-            answer += ((words.indexOf(password.charAt(i))) * words.length())  % 900528;
+        multiplier = 1;
+        for (int i = password.length() - 1; i >= 0; i--) {
+            answer = (answer + (words.indexOf(password.charAt(i))) * multiplier) % module;
+            multiplier = ((words.length())  * multiplier) % module;
         }
-        //abcdedasdacxv
-        //aaa 시작하여
-        //-> b c k 까지 가는데 걸리는 개수
-        //k = 워드 위치(11) + 워드 위치(3) * 문자열 개수 +
-        System.out.println(answer);
+        System.out.println( (answer + 1) % module);
     }
 }
