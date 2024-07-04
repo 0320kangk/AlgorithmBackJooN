150f09f71853c0b422ae4eb296477b9245793817
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2024-07-04 10:52:17 +0900

    수리공 항승 코드 및 풀이

---
+++ src/Glad/Silver/수리공_항승/수리공_항승.java
@@ -0,0 +1,30 @@
+package Glad.Silver.수리공_항승;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 수리공_항승 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int[] arr = new int[N];
+        sc.nextLine();
+        String[] split = sc.nextLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(split[i]);
+        }
+        Arrays.sort(arr);
+        //
+        int startIdx = 0;
+        int count = N;
+        for (int i = 1; i < N; i++) {
+            if(arr[i] - arr[startIdx] + 1 <= L) {
+                count--;
+            } else{
+                startIdx = i;
+            }
+        }
+        System.out.println(count);
+    }
+}
