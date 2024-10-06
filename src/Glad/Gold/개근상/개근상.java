4b0bc8e33da2ea53978aea089ec4bf5d7551aa0d
0320kangk <0320kangk@naver.com>
2024-10-06 10:47:51 +0900

    개근상 코드

---
+++ src/Glad/Gold/개근상/개근상.java
@@ -0,0 +1,17 @@
+package Glad.Gold.개근상;
+
+import java.util.Scanner;
+
+public class 개근상 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        /*
+        총 N일에
+        기본 NCN-2 +
+         */
+
+    }
+}
