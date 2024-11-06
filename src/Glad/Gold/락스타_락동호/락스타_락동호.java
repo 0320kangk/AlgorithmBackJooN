f198615e7846d3139d45312d31209f65b0a120ba
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2024-11-06 10:31:32 +0900

    락스타 동호회 코드 및 풀이

--- src/Glad/Gold/락스타_락동호/락스타_락동호.java
+++ src/Glad/Gold/락스타_락동호/락스타_락동호.java
@@ -9,29 +9,24 @@ public class 락스타_락동호 {
         int FS = sc.nextInt();
         int SF = sc.nextInt();
         int SS = sc.nextInt();
-
         int count = 0;
-
         /*
-        FF ->  FS -> SS -> SF (순환이 될 때)
-
-        SS -> SF (순환이 안될 때)
-
-        FF -> FS (순환이 안될 때)
-
         1. FF 가 0보다 크고 FS=0 이면
         답은 FF
 
         2. FF FS == 0 일 때
-        SS + Math.min(SS,SF);
+        SS + (1 OR 0)
+        SS SS SS SS SS SF
 
-        3. 그 외?
-
-
-
+        3. 그 외
+        3-1 FF -> FS -> SS -> SF 를 통한 순환
+        3-2 FS SF 반복 순환
+        3-3 SS 순환
          */
-        int min = Math.min(FF, Math.min(FS, Math.min(SS, SF)));
-        // FF ->  FS -> SS -> SF (순환이 될 때)
-
+        if(FF > 0 && FS == 0) count = FF;
+        else if( FF == 0 &&  FS == 0) count = SS + Math.min(SF, 1);
+        else if (FS > SF) count = FF + SF * 2 + SS + 1;
+        else count = FF + FS * 2  + SS;
+        System.out.println(count);
     }
 }
