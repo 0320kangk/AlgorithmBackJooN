a8a49f1c46d0f505be36abee41615c6faef38408
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2024-12-09 17:26:24 +0900

    가지고 노는 1 코드

--- src/Glad/Gold/가지고_노는_1/가지고_노는_1.java
+++ src/Glad/Gold/가지고_노는_1/가지고_노는_1.java
@@ -5,8 +5,6 @@ public class 가지고_노는_1 {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         int N = sc.nextInt();
 
-        // Queue는 나머지와 자릿수를 저장
-        // 나머지가 동일한 값을 다시 계산하지 않도록 하기 위한 배열
         boolean[] visited = new boolean[N];
         int remainder = 1 % N;
         int length = 1;
