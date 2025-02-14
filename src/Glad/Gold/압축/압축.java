57b6df6887017106e99ef181ae92dc9e79db01de
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2025-02-14 16:52:44 +0900

    압축 코드 및 풀이

--- src/Glad/Gold/압축/압축.java
+++ src/Glad/Gold/압축/압축.java
@@ -4,60 +4,31 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class 압축 {
+    static String[] splitStr;
+    static int idx;
+    static Stack<String> stack = new Stack<String>();
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        String S = sc.nextLine();
-        Stack<String> stack = new Stack<>();
-        int qLength = 0;
-        for (int i = 0; i < S.length(); i++) {
-            StringBuilder sb = new StringBuilder();
-            char c = S.charAt(i);
-            while(( c >= '0' && c <= '9')){
-                sb.append(c);
-                c= S.charAt(++i);
-            }
-            if(sb.length() > 0) stack.add(sb.toString());
-            if (c == '(') stack.add(String.valueOf(c));
-            else {
-                if(qLength == 0) {
-                    qLength = stack.pop().length();
-                    stack.pop();
-                }
-                String K = stack.pop();
-                int r = Integer.parseInt(K) % 10;
-                qLength *= r;
-                qLength += K.length() - 1;
-            }
+        splitStr = sc.nextLine().split("");
+        System.out.println(decord(0));
+    }
 
+
+    static int decord(int start){
+        int len = 0;
+        for (int i = start; i < splitStr.length ;i++) {
+            if (splitStr[i].equals("(")) {
+                stack.add("(");
+                len += Integer.parseInt(splitStr[i-1]) * decord(i + 1)  - 1;
+                i = idx;
+            } else if (splitStr[i].equals(")")){
+                stack.pop();
+                idx = i;
+                return len;
+            } else {
+                len++;
+            }
         }
-        System.out.println(qLength);
-
-        /*
-        71(9) = 79
-        562(79)
-        33(567979)
-        3 567979 567979 567979
-
-        --스택
-        9
-        (
-        71
-        (
-        562
-        (
-        33
-        --
-        ')' 만나면 9 꺼내기
-        초기값이 없다면 한번더 꺼내기
-        left = 71 꺼내기
-        right = 9
-        7
-
-         567979
-
-         */
-
-
+        return len;
     }
 }
